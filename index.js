99fea913e2f5e07936c6624751e3f994a41b7c4a
arodriguez133 <arodriguez133@icloud.com>
2022-03-31 00:59:42 -0500

    car

--- index.js
+++ index.js
@@ -118,7 +118,9 @@ class Car {
    */
   constructor(name, tankSize, mpg) {
     this.odometer = 0 // car initilizes with zero miles
-    this.tank = tankSize // car initiazes full of gas
+    this.tank = tankSize
+    this.tankSize = tankSize
+    this.mpg = mpg// car initiazes full of gas
     // ✨ initialize whatever other properties are needed
   }
 
@@ -136,7 +138,15 @@ class Car {
    * focus.drive(200) // returns 600 (ran out of gas after 100 miles)
    */
   drive(distance) {
-    // ✨ implement
+    const maximumMiles = this.tank * this.mpg
+    if (distance <= maximumMiles) {
+      this.odometer = this.odometer + distance
+      this.tank = this.tank - (distance / this.mpg)
+      return this.odometer
+    }
+    this.odometer = this.odometer + maximumMiles
+    this.tank = 0
+    return this.odometer
   }
 
   /**
@@ -151,7 +161,13 @@ class Car {
    * focus.refuel(99) // returns 600 (tank only holds 20)
    */
   refuel(gallons) {
-    // ✨ implement
+    const fillItUp = this.tankSize - this.tank
+    if (gallons <= fillItUp) {
+      this.tank = this.tank + gallons
+    } else {
+      this.tank = this.tankSize
+    }
+    return this.tank * this.mpg
   }
 }
 
